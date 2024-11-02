8a2ad862954360d655b82181445d9629612ed34d
shloksy <162043636+shloksy@users.noreply.github.com>
2024-11-01 21:33:52 -0400

    Update test.py

--- test.py
+++ test.py
@@ -4,3 +4,8 @@ import matplotlib.pyplot as plt
 import math
 
 st.title("Duke AI Hackathon test")
+
+if st.button("New Users"):
+    st.session_state["button_clicked"] = "new"
+elif st.button("Returning Users"):
+    st.session_state["button_clicked"] = "returning"
