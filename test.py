301238eae92b64f6230a0012a5adc9f5196eea39
shloksy <162043636+shloksy@users.noreply.github.com>
2024-11-01 22:16:35 -0400

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ st.title("Name of App")
 st.write("Enter your child's information below:")
 name = st.text_input("Enter your child's name:")
 grade = st.selectbox("Select their grade:", ["3rd","4th","5th","6th","7th","8th"])
-subject = st.selectbox("Which subject would you like to focus on?", ["English Comprehension","Math"]
+subject = st.selectbox("Which subject would you like to focus on?", ["English Comprehension","Math"])
 
 st.write("If you would like to recieve progress reports, enter your information below:")
 parent_name = st.text_input("Enter your name:")
