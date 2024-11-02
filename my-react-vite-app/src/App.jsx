2e8cf3834f8407b23437f6d2df3e07ef1b432954
shlok-yeolekar <162043636+shloksy@users.noreply.github.com>
2024-11-02 14:43:23 -0400

    fixes

--- my-react-vite-app/src/App.jsx
+++ my-react-vite-app/src/App.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import logo from '/logo.jpeg'; // Assuming the logo is in the public folder
 import './App.css';
 
-
 const LearnLeap = () => {
   const initialState = {
     childName: '',
@@ -117,89 +116,75 @@ const LearnLeap = () => {
             </div>
           </div>
         )}
+
         {page === 'newUser' && !isFormSubmitted && (
-            <form onSubmit={handleSubmit}>
-              <h2 className="text-3xl font-bold text-darkgreen-500 mb-6">Get Started</h2>
-              <div className="mb-6 text-left">
-                <label className="block text-lg text-gray-600 mb-2" htmlFor="childName">Child's Name:</label>
-                <input className="block w-full p-3 border border-gray-300 rounded" type="text" id="childName"
-                       value={childName} onChange={(e) => setChildName(e.target.value)}/>
-              </div>
-              <div className="mb-6 text-left">
-                <label className="block text-lg text-gray-600 mb-2" htmlFor="gradeLevel">Grade Level:</label>
-                <select className="block w-full p-3 border border-gray-300 rounded" id="gradeLevel" value={gradeLevel}
-                        onChange={(e) => setGradeLevel(e.target.value)}>
-                  <option value="">Select Grade Level</option>
-                  <option value="3">Grade 3</option>
-                  <option value="4">Grade 4</option>
-                  <option value="5">Grade 5</option>
-                  <option value="6">Grade 6</option>
-                  <option value="7">Grade 7</option>
-                  <option value="8">Grade 8</option>
-                </select>
-              </div>
-              <div className="mb-6 text-left">
-                <label className="block text-lg text-gray-600 mb-2" htmlFor="parentName">Parent's Name:</label>
-                <input className="block w-full p-3 border border-gray-300 rounded" type="text" id="parentName"
-                       value={parentName} onChange={(e) => setParentName(e.target.value)}/>
-              </div>
-              <div className="mb-6 text-left">
-                <label className="block text-lg text-gray-600 mb-2" htmlFor="parentEmail">Parent's Email:</label>
-                <input className="block w-full p-3 border border-gray-300 rounded" type="email" id="parentEmail"
-                       value={parentEmail} onChange={(e) => setParentEmail(e.target.value)}/>
-              </div>
-              <div className="mb-6 text-left">
-                <label className="block text-lg text-gray-600 mb-2" htmlFor="password">Password:</label>
-                <input className="block w-full p-3 border border-gray-300 rounded" type="password" id="password"
-                       value={password} onChange={(e) => setPassword(e.target.value)}/>
-              </div>
-              <button
-                  className="button-hover-effect bg-gray-500 hover:bg-gray-700 text-white font-bold py-3 px-6 rounded mt-4"
-                  type="button" onClick={() => setPage('selectUserType')}>Back
-              </button>
-              <button
-                  className="button-hover-effect bg-darkgreen-500 hover:bg-darkgreen-700 text-white font-bold py-3 px-6 rounded"
-                  type="submit">Submit
-              </button>
-            </form>
+          <form onSubmit={handleSubmit}>
+            <h2 className="text-3xl font-bold text-darkgreen-500 mb-6">Get Started</h2>
+            <div className="mb-6 text-left">
+              <label className="block text-lg text-gray-600 mb-2" htmlFor="childName">Child's Name:</label>
+              <input className="block w-full p-3 border border-gray-300 rounded" type="text" id="childName" value={childName} onChange={(e) => setChildName(e.target.value)} />
+            </div>
+            <div className="mb-6 text-left">
+              <label className="block text-lg text-gray-600 mb-2" htmlFor="gradeLevel">Grade Level:</label>
+              <select className="block w-full p-3 border border-gray-300 rounded" id="gradeLevel" value={gradeLevel} onChange={(e) => setGradeLevel(e.target.value)}>
+                <option value="">Select Grade Level</option>
+                <option value="3">Grade 3</option>
+                <option value="4">Grade 4</option>
+                <option value="5">Grade 5</option>
+                <option value="6">Grade 6</option>
+                <option value="7">Grade 7</option>
+                <option value="8">Grade 8</option>
+              </select>
+            </div>
+            <div className="mb-6 text-left">
+              <label className="block text-lg text-gray-600 mb-2" htmlFor="parentName">Parent's Name:</label>
+              <input className="block w-full p-3 border border-gray-300 rounded" type="text" id="parentName" value={parentName} onChange={(e) => setParentName(e.target.value)} />
+            </div>
+            <div className="mb-6 text-left">
+              <label className="block text-lg text-gray-600 mb-2" htmlFor="parentEmail">Parent's Email:</label>
+              <input className="block w-full p-3 border border-gray-300 rounded" type="email" id="parentEmail" value={parentEmail} onChange={(e) => setParentEmail(e.target.value)} />
+            </div>
+            <div className="mb-6 text-left">
+              <label className="block text-lg text-gray-600 mb-2" htmlFor="password">Password:</label>
+              <input className="block w-full p-3 border border-gray-300 rounded" type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </div>
+            <div className="flex justify-center space-x-4">
+              <button className="button-hover-effect bg-gray-500 hover:bg-gray-700 text-white font-bold py-3 px-6 rounded" type="button" onClick={() => setPage('selectUserType')}>Back</button>
+              <button className="button-hover-effect bg-darkgreen-500 hover:bg-darkgreen-700 text-white font-bold py-3 px-6 rounded" type="submit">Submit</button>
+            </div>
+          </form>
         )}
 
         {page === 'returningUser' && (
-            <form onSubmit={handleSignIn}>
-              <h2 className="text-3xl font-bold text-darkgreen-500 mb-6">Sign In</h2>
-              {errorMessage && <p className="text-red-500 mb-4">{errorMessage}</p>}
-              <div className="mb-6 text-left">
-                <label className="block text-lg text-gray-600 mb-2" htmlFor="parentEmail">Parent's Email:</label>
-                <input className="block w-full p-3 border border-gray-300 rounded" type="email" id="parentEmail"
-                       value={parentEmail} onChange={(e) => setParentEmail(e.target.value)}/>
-              </div>
-              <div className="mb-6 text-left">
-                <label className="block text-lg text-gray-600 mb-2" htmlFor="password">Password:</label>
-                <input className="block w-full p-3 border border-gray-300 rounded" type="password" id="password"
-                       value={password} onChange={(e) => setPassword(e.target.value)}/>
-              </div>
-              <button
-                  className="button-hover-effect bg-gray-500 hover:bg-gray-700 text-white font-bold py-3 px-6 rounded mt-4"
-                  type="button" onClick={() => setPage('selectUserType')}>Back
-              </button>
-
-              <button
-                  className="button-hover-effect bg-darkgreen-500 hover:bg-darkgreen-700 text-white font-bold py-3 px-6 rounded"
-                  type="submit">Sign In
-              </button>
-            </form>
+          <form onSubmit={handleSignIn}>
+            <h2 className="text-3xl font-bold text-darkgreen-500 mb-6">Sign In</h2>
+            {errorMessage && <p className="text-red-500 mb-4">{errorMessage}</p>}
+            <div className="mb-6 text-left">
+              <label className="block text-lg text-gray-600 mb-2" htmlFor="parentEmail">Parent's Email:</label>
+              <input className="block w-full p-3 border border-gray-300 rounded" type="email" id="parentEmail" value={parentEmail} onChange={(e) => setParentEmail(e.target.value)} />
+            </div>
+            <div className="mb-6 text-left">
+              <label className="block text-lg text-gray-600 mb-2" htmlFor="password">Password:</label>
+              <input className="block w-full p-3 border border-gray-300 rounded" type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </div>
+            <div className="flex justify-center space-x-4">
+              <button className="button-hover-effect bg-gray-500 hover:bg-gray-700 text-white font-bold py-3 px-6 rounded" type="button" onClick={() => setPage('selectUserType')}>Back</button>
+              <button className="button-hover-effect bg-darkgreen-500 hover:bg-darkgreen-700 text-white font-bold py-3 px-6 rounded" type="submit">Sign In</button>
+            </div>
+          </form>
         )}
 
         {page === 'selectSubject' && (
-            <div>
-              <h2 className="text-3xl font-bold text-darkgreen-500 mb-6">Select Subject</h2>
-              <div className="flex flex-col space-y-4">
+          <div>
+            <h2 className="text-3xl font-bold text-darkgreen-500 mb-6">Select Subject</h2>
+            <div className="flex flex-col space-y-4">
               <button className="button-hover-effect bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded" onClick={() => handleSubjectSelect('english')}>English</button>
               <button className="button-hover-effect bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-6 rounded" onClick={() => handleSubjectSelect('math')}>Math</button>
               <button className="button-hover-effect bg-red-500 hover:bg-red-700 text-white font-bold py-3 px-6 rounded" onClick={resetState}>Log Out</button>
             </div>
           </div>
         )}
+
         {page === 'subjectPractice' && (
           <div>
             <h2 className="text-3xl font-bold text-darkgreen-500 mb-1">{subject === 'english' ? 'English Practice' : 'Math Practice'}</h2>
@@ -217,4 +202,3 @@ const LearnLeap = () => {
 };
 
 export default LearnLeap;
-
