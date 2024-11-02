ce4a1cbc0f543b9a4872b88d5b5f4585f418468e
shloksy <162043636+shloksy@users.noreply.github.com>
2024-11-01 21:53:12 -0400

    Update test.py

--- test.py
+++ test.py
@@ -18,3 +18,10 @@ elif st.session_state["user_type"] == "Returning User":
 
 # Display current selection for feedback
 st.write(f"Currently selected: {st.session_state['user_type']}")
+
+
+st.title("Name of App")
+st.write("Enter your child's information below:")
+st.write("If you would like to recieve progress reports, enter your information below:")
+parent_name = st.text_input("Enter your name:")
+parent_email = st.text_input("Enter your email:")
